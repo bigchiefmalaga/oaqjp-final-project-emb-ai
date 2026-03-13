c6735b5603dc00f48fb743351f231e0a3a055090
bigchiefmalaga <afsahi.ma@gmail.com>
2026-03-13 19:42:09 -0400

    EmotionDetection package complet

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -2,15 +2,15 @@ import unittest
 
 from emotion_detection import emotion_detector
 class TestEmotion(unittest.TestCase):
-    def test_joy(self)
+    def test_joy(self):
     self assertEqual(emtion_detector("I am glad this happened"), 'Joy')
-    def test_anger(self)
+    def test_anger(self):
     self assertEqual(emtion_detector("I am really mad about this"), 'anger')
-    def test_disgust(self)
+    def test_disgust(self):
     self assertEqual(emtion_detector("I feel disgusted just hearing about this"), 'disgust')
-    def test_sadness(self)
+    def test_sadness(self):
     self assertEqual(emtion_detector("I am so sad about this"), 'sadness')
-    def test_fear(self)
+    def test_fear(self):
     self assertEqual(emtion_detector("I am really afraid that this will happen"), 'fear')
 
     if __name__ == '__main__':
