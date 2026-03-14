595aa7dfed297ebde5b8d19dc7c29642afade566
bigchiefmalaga <afsahi.ma@gmail.com>
2026-03-14 17:33:46 -0400

    Test Commit

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,17 +1,18 @@
 import unittest
 
 from emotion_detection import emotion_detector
+
 class TestEmotion(unittest.TestCase):
     def test_joy(self):
-    self assertEqual(emtion_detector("I am glad this happened"), 'Joy')
+        self.assertEqual(emotion_detector("I am glad this happened"), 'joy')
     def test_anger(self):
-    self assertEqual(emtion_detector("I am really mad about this"), 'anger')
+        self.assertEqual(emotion_detector("I am really mad about this"), 'anger')
     def test_disgust(self):
-    self assertEqual(emtion_detector("I feel disgusted just hearing about this"), 'disgust')
+        self.assertEqual(emotion_detector("I feel disgusted just hearing about this"), 'disgust')
     def test_sadness(self):
-    self assertEqual(emtion_detector("I am so sad about this"), 'sadness')
+        self.assertEqual(emotion_detector("I am so sad about this"), 'sadness')
     def test_fear(self):
-    self assertEqual(emtion_detector("I am really afraid that this will happen"), 'fear')
+        self.assertEqual(emotion_detector("I am really afraid that this will happen"), 'fear')
 
-    if __name__ == '__main__':
-        unittest.main()
+if __name__ == '__main__':
+    unittest.main()
